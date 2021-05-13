543a91ec560154d2539b2c28eaf72388ecdc7f02
Dave Byron <dbyron@chghealthcare.com>
2021-05-13 15:30:37 -0600

    test merge of pub and sub into one service

--- pi-mqtt-service.py
+++ pi-mqtt-service.py
@@ -35,15 +35,18 @@ def on_message(client, userdata, msg):
     print(f'Topic: {msg.topic}\nMessage: {msg.payload.decode()}')
     previous_state = ''
     if msg.payload.decode() == '0':
-        print('off')
+        print('state set to \'off\'')
         set_gpio_state(pin=gpio_pin, state=GPIO.LOW)
         # GPIO.output(gpio_pin, state)
+
     if msg.payload.decode() == '1':
-        print('on')
+        print('state set tp \'on\'')
         set_gpio_state(pin=gpio_pin, state=GPIO.HIGH)
         # GPIO.output(gpio_pin, GPIO.HIGH)
+
     if previous_state != get_gpio_state(pin=gpio_pin):
         client.publish(mqtt_status_topic, get_gpio_state(pin=gpio_pin))
+        print(f'published \'{get_gpio_state(pin=gpio_pin)}\' to \'{mqtt_status_topic}\'')
         previous_state = get_gpio_state(pin=gpio_pin)
 
 
