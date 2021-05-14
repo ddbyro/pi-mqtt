c4b863c048204bb92cd403591119d105436de1fc
Dave Byron <dbyron@chghealthcare.com>
2021-05-13 19:16:16 -0600

    test merge of pub and sub into one service

--- pi-mqtt-service.py
+++ pi-mqtt-service.py
@@ -7,10 +7,10 @@ config = yaml.full_load(open('./config.yaml'))
 
 mqtt_broker = config['configs']['host']
 mqtt_port = config['configs']['port']
-mqtt_set_topic = config['configs']['relays']['relay01']['set_topic']
-mqtt_status_topic = config['configs']['relays']['relay01']['status_topic']
+# mqtt_set_topic = config['configs']['relays']['relay01']['set_topic']
+# mqtt_status_topic = config['configs']['relays']['relay01']['status_topic']
 
-gpio_pin = config['configs']['relays']['relay01']['pin']
+# gpio_pin = config['configs']['relays']['relay01']['pin']
 
 
 def set_gpio_state(pin=None, state=None):
@@ -24,36 +24,46 @@ def get_gpio_state(pin=None):
 def on_connect(client, userdata, flags, rc):
     print(f'Connected with result code {str(rc)}')
     # Subscribing to receive RPC requests
-    client.subscribe(mqtt_set_topic)
+    # client.subscribe(mqtt_set_topic)
 
 
 def on_publish(client, userdata, mid):
-    print(f'published \'{get_gpio_state(pin=gpio_pin)}\' to \'{mqtt_status_topic}\'')
+
+    print(f'published')  # \'{get_gpio_state(pin=gpio_pin)}\' to \'{mqtt_status_topic}\'')
 
 
 def on_message(client, userdata, msg):
     #print(f'Topic {msg.topic} Message: {msg.payload.decode()}')
-    previous_state = ''
-    if msg.payload.decode() == '0':
-        print('state set to \'off\'')
-        set_gpio_state(pin=gpio_pin, state=GPIO.LOW)
-        # GPIO.output(gpio_pin, state)
+    for relay in config['relays']:
+        name = relay['name']
+        gpio_pin = relay['pin']
+        mqtt_set_topic = relay['set_topic']
+        mqtt_status_topic = relay['status_topic']
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(gpio_pin, GPIO.OUT)
 
-    if msg.payload.decode() == '1':
-        print('state set to \'on\'')
-        set_gpio_state(pin=gpio_pin, state=GPIO.HIGH)
-        # GPIO.output(gpio_pin, GPIO.HIGH)
+        client.subscribe(mqtt_set_topic)
 
-    if previous_state != get_gpio_state(pin=gpio_pin):
-        client.publish(mqtt_status_topic, get_gpio_state(pin=gpio_pin))
-        print(f'published \'{get_gpio_state(pin=gpio_pin)}\' to \'{mqtt_status_topic}\'')
-        previous_state = get_gpio_state(pin=gpio_pin)
+        previous_state = ''
+        if msg.payload.decode() == '0':
+            print('state set to \'off\'')
+            set_gpio_state(pin=gpio_pin, state=GPIO.LOW)
+            # GPIO.output(gpio_pin, state)
+
+        if msg.payload.decode() == '1':
+            print('state set to \'on\'')
+            set_gpio_state(pin=gpio_pin, state=GPIO.HIGH)
+            # GPIO.output(gpio_pin, GPIO.HIGH)
+
+        if previous_state != get_gpio_state(pin=gpio_pin):
+            client.publish(mqtt_status_topic, get_gpio_state(pin=gpio_pin))
+            print(f'published \'{get_gpio_state(pin=gpio_pin)}\' to \'{mqtt_status_topic}\'')
+            previous_state = get_gpio_state(pin=gpio_pin)
 
 
 def connect_mqtt():
-    GPIO.setwarnings(False)
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(gpio_pin, GPIO.OUT)
+
     client = mqtt.Client()
     client.on_connect = on_connect
     client.on_message = on_message
--- test_confi.py
+++ test_confi.py
@@ -3,8 +3,15 @@ import yaml
 
 config = yaml.full_load(open('./config.yaml'))
 
+# rela
+# num_relays = len(config['relays'].keys())
+for relay in config['relays']:
+    name = relay['name']
+    pin = relay['pin']
+    set_topic = relay['set_topic']
+    status_topic = relay['status_topic']
+    print(f'relay {name} is on pin {pin}, its topics are {set_topic} and {status_topic}')
+# print(config['relays'][f'relay0{num_relays}'])
 
-# print(config)
-for config, relays in config.items():
-
-     print(relays)
+# for cfg in config:
+#      print(cfg)
