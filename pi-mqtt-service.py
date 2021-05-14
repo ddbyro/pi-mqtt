eb19462c204d746611c11c35fd3ab60718401725
Dave Byron <dbyron@chghealthcare.com>
2021-05-13 20:06:58 -0600

    test merge of pub and sub into one service

--- pi-mqtt-service.py
+++ pi-mqtt-service.py
@@ -12,70 +12,69 @@ mqtt_port = config['broker_configs']['port']
 # mqtt_status_topic = config['configs']['relays']['relay01']['status_topic']
 
 # gpio_pin = config['configs']['relays']['relay01']['pin']
+for relay in config['relays']:
+    name = relay['name']
+    gpio_pin = relay['pin']
+    mqtt_set_topic = relay['set_topic']
+    mqtt_status_topic = relay['status_topic']
+
+    def set_gpio_state(pin=None, state=None):
+        GPIO.output(pin, state)
 
 
-def set_gpio_state(pin=None, state=None):
-    GPIO.output(pin, state)
-
-
-def get_gpio_state(pin=None):
-    return GPIO.input(pin)
-
-
-
-def on_connect(client, userdata, flags, rc, mqtt_set_topic=None):
-    print(f'Connected with result code {str(rc)}')
-    # Subscribing to receive RPC requests
-    client.subscribe(mqtt_set_topic)
-
-
-def on_publish(client, userdata, mid):
-
-    print(f'published')  # \'{get_gpio_state(pin=gpio_pin)}\' to \'{mqtt_status_topic}\'')
-
-
-def on_message(client, userdata, msg, gpio_pin=None, mqtt_status_topic=None):
-    #print(f'Topic {msg.topic} Message: {msg.payload.decode()}')
-
-    if msg.payload.decode() == '0':
-        print('state set to \'off\'')
-        set_gpio_state(pin=gpio_pin, state=GPIO.LOW)
-        # GPIO.output(gpio_pin, state)
-
-    if msg.payload.decode() == '1':
-        print('state set to \'on\'')
-        set_gpio_state(pin=gpio_pin, state=GPIO.HIGH)
-        # GPIO.output(gpio_pin, GPIO.HIGH)
-
-    previous_state = ''
-    if previous_state != get_gpio_state(pin=gpio_pin):
-        client.publish(mqtt_status_topic, get_gpio_state(pin=gpio_pin))
-        print(f'published \'{get_gpio_state(pin=gpio_pin)}\' to \'{mqtt_status_topic}\'')
-        previous_state = get_gpio_state(pin=gpio_pin)
+    def get_gpio_state(pin=None):
+        return GPIO.input(pin)
 
 
 
-def connect_mqtt():
-    GPIO.setwarnings(False)
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(gpio_pin, GPIO.OUT)
-    client = mqtt.Client()
-    client.on_connect = on_connect
-    client.on_message = on_message
-    client.connect(mqtt_broker, mqtt_port, 60)
-    client.loop_forever()
+    def on_connect(client, userdata, flags, rc, mqtt_set_topic=None):
+        print(f'Connected with result code {str(rc)}')
+        # Subscribing to receive RPC requests
+        client.subscribe(mqtt_set_topic)
+
+
+    def on_publish(client, userdata, mid):
+
+        print(f'published')  # \'{get_gpio_state(pin=gpio_pin)}\' to \'{mqtt_status_topic}\'')
+
+
+    def on_message(client, userdata, msg, gpio_pin=None, mqtt_status_topic=None):
+        #print(f'Topic {msg.topic} Message: {msg.payload.decode()}')
+
+        if msg.payload.decode() == '0':
+            print('state set to \'off\'')
+            set_gpio_state(pin=gpio_pin, state=GPIO.LOW)
+            # GPIO.output(gpio_pin, state)
+
+        if msg.payload.decode() == '1':
+            print('state set to \'on\'')
+            set_gpio_state(pin=gpio_pin, state=GPIO.HIGH)
+            # GPIO.output(gpio_pin, GPIO.HIGH)
+
+        previous_state = ''
+        if previous_state != get_gpio_state(pin=gpio_pin):
+            client.publish(mqtt_status_topic, get_gpio_state(pin=gpio_pin))
+            print(f'published \'{get_gpio_state(pin=gpio_pin)}\' to \'{mqtt_status_topic}\'')
+            previous_state = get_gpio_state(pin=gpio_pin)
+
+
+
+    def connect_mqtt():
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(gpio_pin, GPIO.OUT)
+        client = mqtt.Client()
+        client.on_connect = on_connect
+        client.on_message = on_message
+        client.connect(mqtt_broker, mqtt_port, 60)
+        client.loop_forever()
 
 
 def main():
-    for relay in config['relays']:
-        name = relay['name']
-        gpio_pin = relay['pin']
-        mqtt_set_topic = relay['set_topic']
-        mqtt_status_topic = relay['status_topic']
-        try:
-            connect_mqtt()
-        except KeyboardInterrupt:
-            GPIO.cleanup()
+    try:
+        connect_mqtt()
+    except KeyboardInterrupt:
+        GPIO.cleanup()
 
 
 if __name__ == '__main__':
