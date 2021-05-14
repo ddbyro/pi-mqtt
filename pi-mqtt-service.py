d59191a3fd1b2a8e1b3b60f3ac7b7f562447f537
Dave Byron <dbyron@chghealthcare.com>
2021-05-13 19:28:12 -0600

    test merge of pub and sub into one service

--- pi-mqtt-service.py
+++ pi-mqtt-service.py
@@ -24,7 +24,7 @@ def get_gpio_state(pin=None):
 def on_connect(client, userdata, flags, rc):
     print(f'Connected with result code {str(rc)}')
     # Subscribing to receive RPC requests
-    # client.subscribe(mqtt_set_topic)
+    client.subscribe(mqtt_set_topic)
 
 
 def on_publish(client, userdata, mid):
@@ -34,40 +34,38 @@ def on_publish(client, userdata, mid):
 
 def on_message(client, userdata, msg):
     #print(f'Topic {msg.topic} Message: {msg.payload.decode()}')
+
+    if msg.payload.decode() == '0':
+        print('state set to \'off\'')
+        set_gpio_state(pin=gpio_pin, state=GPIO.LOW)
+        # GPIO.output(gpio_pin, state)
+
+    if msg.payload.decode() == '1':
+        print('state set to \'on\'')
+        set_gpio_state(pin=gpio_pin, state=GPIO.HIGH)
+        # GPIO.output(gpio_pin, GPIO.HIGH)
+
+    previous_state = ''
+    if previous_state != get_gpio_state(pin=gpio_pin):
+        client.publish(mqtt_status_topic, get_gpio_state(pin=gpio_pin))
+        print(f'published \'{get_gpio_state(pin=gpio_pin)}\' to \'{mqtt_status_topic}\'')
+        previous_state = get_gpio_state(pin=gpio_pin)
+
+
+def connect_mqtt():
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
     for relay in config['relays']:
         name = relay['name']
         gpio_pin = relay['pin']
         mqtt_set_topic = relay['set_topic']
         mqtt_status_topic = relay['status_topic']
         GPIO.setup(gpio_pin, GPIO.OUT)
-
-        client.subscribe(mqtt_set_topic)
-
-        if msg.payload.decode() == '0':
-            print('state set to \'off\'')
-            set_gpio_state(pin=gpio_pin, state=GPIO.LOW)
-            # GPIO.output(gpio_pin, state)
-
-        if msg.payload.decode() == '1':
-            print('state set to \'on\'')
-            set_gpio_state(pin=gpio_pin, state=GPIO.HIGH)
-            # GPIO.output(gpio_pin, GPIO.HIGH)
-
-        previous_state = ''
-        if previous_state != get_gpio_state(pin=gpio_pin):
-            client.publish(mqtt_status_topic, get_gpio_state(pin=gpio_pin))
-            print(f'published \'{get_gpio_state(pin=gpio_pin)}\' to \'{mqtt_status_topic}\'')
-            previous_state = get_gpio_state(pin=gpio_pin)
-
-
-def connect_mqtt():
-    GPIO.setwarnings(False)
-    GPIO.setmode(GPIO.BCM)
-    client = mqtt.Client()
-    client.on_connect = on_connect
-    client.on_message = on_message
-    client.connect(mqtt_broker, mqtt_port, 60)
-    client.loop_forever()
+        client = mqtt.Client()
+        client.on_connect = on_connect
+        client.on_message = on_message
+        client.connect(mqtt_broker, mqtt_port, 60)
+        client.loop_forever()
 
 
 def main():
