e46f2d548e1403d004b62ef30a7f10ba3ee7105b
ddbyro@gmail.com <ddbyro@gmail.com>
2021-05-15 00:37:22 -0600

    added setup.sh

---
+++ setup.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+pip3 install -r requirements.txt
+
+mkdir -p /opt/pi-mqtt-service/
+
+mv ./pi-mqtt-service.py /usr/bin/
+mv ./config /opt/pi-mqtt-service/
+
+cat > /etc/systemd/system/pi-mqtt-service.service <<EOF
+[Unit]
+Description=GPIO MQTT
+After=network.target
+
+[Service]
+Type=simple
+User=root
+ExecStart=/usr/bin/python3 /pi-mqtt-service.py -p /opt/pi-mqtt-service/config.yaml
+Restart=on-failure
+RestartSec=5s
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+
+
