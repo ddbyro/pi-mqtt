4373d487efbb4a9496dfcd030583d4671f39f8f4
ddbyro@gmail.com <ddbyro@gmail.com>
2021-05-14 23:50:29 -0600

    refactores nameing

--- pi-mqtt-service.py
+++ pi-mqtt-service.py
@@ -8,6 +8,7 @@ config = yaml.full_load(open('./config.yaml'))
 
 mqtt_broker = config['broker_configs']['host']
 mqtt_port = config['broker_configs']['port']
+mqtt_topic = config["broker_configs"]["topic"]
 gpio_configs = config["gpio_configs"]
 
 def set_gpio_state(pin=None, state=None):
@@ -21,7 +22,7 @@ def get_gpio_state(pin=None):
 def on_connect(client, userdata, flags, rc):
     print(f'Connected with result code {str(rc)}')
     for gpio in gpio_configs:
-        client.subscribe(f'{config["broker_configs"]["topic"]}/{gpio["id"]}/state')
+        client.subscribe(f'{mqtt_topic}/{gpio["id"]}/state')
 
 
 def on_message(client, userdata, msg):
@@ -38,16 +39,16 @@ def on_message(client, userdata, msg):
             if topic_message == 'off':
                 GPIO.output(gpio_pin, GPIO.LOW)
                 print(f'gpio pi {gpio_pin} state set to \'off\'')
-                client.publish(f'{config["broker_configs"]["topic"]}/{gpio["id"]}/status', get_gpio_state(pin=gpio_pin))
+                client.publish(f'{mqtt_topic}/{gpio["id"]}/status', get_gpio_state(pin=gpio_pin))
                 if get_gpio_state(pin=gpio_pin) == 0:
-                    client.publish(f'{config["broker_configs"]["topic"]}/{gpio["id"]}/status', 'off')
+                    client.publish(f'{mqtt_topic}/{gpio["id"]}/status', 'off')
 
             if topic_message == 'on':
                 GPIO.output(gpio_pin, GPIO.HIGH)
                 print(f'gpio pi {gpio_pin} state set to \'on\'')
-                client.publish(f'{config["broker_configs"]["topic"]}/{gpio["id"]}/status', get_gpio_state(pin=gpio_pin))
+                client.publish(f'{mqtt_topic}/{gpio["id"]}/status', get_gpio_state(pin=gpio_pin))
                 if get_gpio_state(pin=gpio_pin) == 1:
-                    client.publish(f'{config["broker_configs"]["topic"]}/{gpio["id"]}/status', 'on')
+                    client.publish(f'{mqtt_topic}/{gpio["id"]}/status', 'on')
 
 
 def connect_mqtt():
