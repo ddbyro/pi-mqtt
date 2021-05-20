c852c88e1dd641329a258583ec72173167437f15
Dave Byron <dbyron@chghealthcare.com>
2021-05-20 16:28:10 -0600

    updated comments

--- bin/pi-mqtt-service.py
+++ bin/pi-mqtt-service.py
@@ -3,8 +3,9 @@ import paho.mqtt.client as mqtt
 import RPi.GPIO as GPIO
 import yaml
 import argparse
-# from RPiSim.GPIO import GPIO
+# from RPiSim.GPIO import GPIO  # this is used for debugging on a non-raspberry pi machine
 
+# Setup commandline arguments for configs
 parser = argparse.ArgumentParser()
 parser.add_argument('-p', '--path', action='store', dest='path', help='Path to config.yaml.')
 
@@ -13,6 +14,7 @@ config_path = args.path
 
 config = yaml.full_load(open(config_path))
 
+# global variables for configs
 mqtt_broker = config['broker_configs']['host']
 mqtt_port = config['broker_configs']['port']
 mqtt_topic = config["broker_configs"]["topic"]
@@ -20,22 +22,28 @@ gpio_configs = config["gpio_configs"]
 
 
 def set_gpio_state(pin=None, state=None):
+    # set the gpio pin state
     GPIO.setup(pin, state)
 
 
 def get_gpio_state(pin=None):
+    # get the gpio state
     return GPIO.input(pin)
 
 
 def on_connect(client, userdata, flags, rc):
+    # execute on connect to mqtt
     print(f'Connected with result code {str(rc)}')
+    # subscribe to each topic in topic config
     for gpio in gpio_configs:
         client.subscribe(f'{mqtt_topic}/{gpio["id"]}/state')
 
 
 def on_message(client, userdata, msg):
+    # execute on message update to subscribed topics
     print(f'Topic {msg.topic} Message: {msg.payload.decode()}')
 
+    # set pin to published state and publish actual pin state to status topic
     for gpio in gpio_configs:
         gpio_id = msg.topic.split("/")[-2]
         topic_message = msg.payload.decode()
@@ -43,36 +51,55 @@ def on_message(client, userdata, msg):
         if f'{gpio_id}' in gpio["id"]:
             gpio_pin = gpio["pin"]
 
+            # set gpio to to LOW if topic state is 'off' or HIGH if state is 'on'
             if topic_message == 'off':
                 GPIO.output(gpio_pin, GPIO.LOW)
                 print(f'gpio pi {gpio_pin} state set to \'off\'')
+
+                # set gpio to 'off'
                 client.publish(f'{mqtt_topic}/{gpio["id"]}/status', get_gpio_state(pin=gpio_pin))
+
+                # publish pin state
                 if get_gpio_state(pin=gpio_pin) == 0:
                     client.publish(f'{mqtt_topic}/{gpio["id"]}/status', 'off')
 
             if topic_message == 'on':
                 GPIO.output(gpio_pin, GPIO.HIGH)
                 print(f'gpio pi {gpio_pin} state set to \'on\'')
+
+                # set gpio to 'on'
                 client.publish(f'{mqtt_topic}/{gpio["id"]}/status', get_gpio_state(pin=gpio_pin))
+
+                # publish pin state
                 if get_gpio_state(pin=gpio_pin) == 1:
                     client.publish(f'{mqtt_topic}/{gpio["id"]}/status', 'on')
 
 
-def connect_mqtt():
-    GPIO.setwarnings(False)
-    GPIO.setmode(GPIO.BCM)
-    for gpio in gpio_configs:
-        GPIO.setup(gpio['pin'], GPIO.OUT)
-    client = mqtt.Client()
-    client.on_connect = on_connect
-    client.on_message = on_message
-    client.connect(mqtt_broker, mqtt_port, 60)
-    client.loop_forever()
+# def connect_mqtt():  # connection loop for sub
+#     GPIO.setwarnings(False)
+#     GPIO.setmode(GPIO.BCM)
+#     for gpio in gpio_configs:
+#         GPIO.setup(gpio['pin'], GPIO.OUT)
+#     client = mqtt.Client()
+#     client.on_connect = on_connect
+#     client.on_message = on_message
+#     client.connect(mqtt_broker, mqtt_port, 60)
+#     client.loop_forever()
 
 
 def main():
     try:
-        connect_mqtt()
+        # setup gpio
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BCM)
+        for gpio in gpio_configs:
+            GPIO.setup(gpio['pin'], GPIO.OUT)
+        # connection loop for mqtt client
+        client = mqtt.Client()
+        client.on_connect = on_connect  # on connection run the on_connect function
+        client.on_message = on_message  # on message published to subscribed topics run on_message function
+        client.connect(mqtt_broker, mqtt_port, 60)  # connection string for mqtt broker
+        client.loop_forever()  # loop subscriptions forever
     except KeyboardInterrupt:
         GPIO.cleanup()
 
